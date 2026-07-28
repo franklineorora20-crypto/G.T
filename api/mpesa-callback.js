31a6eaed137652ca9aacdc7e64c447eec3bdde6d
Frankline Orora <franklineorora20@gmail.com>
2026-07-28 12:44:54 +0300

    add mpesa callback function logging

--- api/mpesa-callback.js
+++ api/mpesa-callback.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
 
+  console.log("MPESA CALLBACK FUNCTION HIT");
+
   console.log(
     "MPESA CALLBACK DATA:",
     JSON.stringify(req.body, null, 2)
   );
 
-  res.status(200).json({
+  return res.status(200).json({
     ResultCode: 0,
     ResultDesc: "Accepted"
   });
