cc101a60bbea00a3864d1b42b524067929a9f890
Frankline Orora <franklineorora20@gmail.com>
2026-07-28 05:18:10 +0300

    add stkpush debug log

--- api/stkpush.js
+++ api/stkpush.js
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export default async function handler(req, res) {
 
+    console.log("STKPUSH FUNCTION HIT");
+    
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
